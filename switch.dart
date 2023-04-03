80f77581d1ee3da1519d83c43100ce62c4bc6e33
VITORIA GABRIELA DOS SANTOS <vitoria.8990@aluno.pr.senac.br>
2023-04-03 11:56:04 -0300

    vaii

---
+++ switch.dart
@@ -0,0 +1,18 @@
+void main() {
+  String linguagem="html";
+  switch (linguagem) {
+    case "html":
+    print("linguagem de programação legal");
+    break;
+    case "php":
+    print("linguagem de programação super legal");
+    break;
+    case "javascript":
+    print("super legal");
+    break;
+    default:
+    
+    print("não conheço essa linguagem de programação");
+    break;
+  }
+}
