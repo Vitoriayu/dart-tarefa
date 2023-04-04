93a258a96e2081b9c924b74bf6f89c5dcfb2075c
VITORIA GABRIELA DOS SANTOS <vitoria.8990@aluno.pr.senac.br>
2023-04-04 16:33:42 -0300

    oiii

--- primos.dart
+++ primos.dart
@@ -1,7 +1,16 @@
 void main() {
-  int i = 1;
-  while (i <= 1000) {
-    print(i);
-    i++;
+  List<int> primos = [2];
+  for (int i = 3; i <= 1000; i++) {
+    bool eprimo = true;
+    for (int j = 2; j <= i ~/ 2; j++) {
+      if (i % j == 0) {
+        eprimo = false;
+        break;
+      }
+    }
+    if (eprimo) {
+      primos.add(i);
+    }
   }
+  print("numeros primos de 1 a 1000: $primos");
 }
